494afac9903a88a779cebf092e492d9014861e29
shenxinli <2509127733@qq.com>
2022-11-30 05:36:43 +0800

    ✨ feat: 脚手架

---
+++ 02_learn_scaffolds/src/HelloWorld.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class HelloWorld extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>helloworld</h1>
+                <button>hello</button>
+            </div>
+        )
+    }
+}
+
+export default HelloWorld
